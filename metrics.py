70d3881985cb49eab08c501d649374419a068a88
Isha Singhal <isinghal@isinghal-macbookpro.roam.corp.google.com>
2020-02-22 17:35:03 -0800

    Corrected the evaluation metric.

--- metrics.py
+++ metrics.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 
-# Computes mean of (square(logy - logy*))
+# Evaluation metric.
+# Computes sqrt(mean(square(logy - logy*)))
 def log_error(logits, depths, invalid_depths):
     logits_flat = tf.reshape(logits, [-1, 55*74])
     depths_flat = tf.reshape(depths, [-1, 55*74])
@@ -10,6 +11,6 @@ def log_error(logits, depths, invalid_depths):
     d = tf.subtract(predict, target)
     square_d = tf.square(d)
     sum_square_d = tf.reduce_sum(square_d, 1)
-    cost = tf.reduce_mean(sum_square_d / 55.0*74.0)
+    cost = tf.sqrt(tf.reduce_mean(sum_square_d / 55.0*74.0))
     tf.add_to_collection('error', cost)
     return tf.add_n(tf.get_collection('error'), name='total_error')

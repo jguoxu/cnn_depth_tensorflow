d274262215be9e75c254f114f1b3763c80913de9
Isha Singhal <isinghal@isinghal-macbookpro.roam.corp.google.com>
2020-02-22 18:28:32 -0800

    Adds scale invariant error

--- metrics.py
+++ metrics.py
@@ -14,3 +14,19 @@ def log_error(logits, depths, invalid_depths):
     cost = tf.sqrt(tf.reduce_mean(sum_square_d / 55.0*74.0))
     tf.add_to_collection('error', cost)
     return tf.add_n(tf.get_collection('error'), name='total_error')
+
+def scale_invariant_error(logits, depths, invalid_depths):
+    logits_flat = tf.reshape(logits, [-1, 55*74])
+    depths_flat = tf.reshape(depths, [-1, 55*74])
+    invalid_depths_flat = tf.reshape(invalid_depths, [-1, 55*74])
+
+    predict = tf.multiply(logits_flat, invalid_depths_flat)
+    target = tf.multiply(depths_flat, invalid_depths_flat)
+    d = tf.subtract(predict, target)
+    square_d = tf.square(d)
+    sum_square_d = tf.reduce_sum(square_d, 1)
+    sum_d = tf.reduce_sum(d, 1)
+    sqare_sum_d = tf.square(sum_d)
+    cost = tf.sqrt(tf.reduce_mean(sum_square_d / 55.0*74.0 - 0.5*sqare_sum_d / math.pow(55*74, 2)))
+    tf.add_to_collection('losses', cost)
+    return tf.add_n(tf.get_collection('losses'), name='total_loss')
